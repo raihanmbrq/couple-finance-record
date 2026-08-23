814c1c2d231940f36786fa7165256c800feb1e53
raihanmbrq <mubaroqraihan@gmail.com>
2026-08-23 14:06:47 +0700

    Save

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ucnop395n24"
+    "revision": "0.4a1gks946ec"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
